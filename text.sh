bcf16a4077cf2fb27fe9554d3b62f3956c7b0504
IsmailKhalimov <114234533+IsmailKhalimov@users.noreply.github.com>
2022-10-01 09:58:04 +0300

    Add files via upload

---
+++ text.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'The current date is:'
+date
+echo 'The username is:'
+whoami
+echo 'The informasion about user:'
+who
+echo 'The current directory is:'
+pwd
+echo 'There are all Folders'
+ls -l
